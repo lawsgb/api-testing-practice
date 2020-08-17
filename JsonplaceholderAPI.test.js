4884c84edd7e63aae2f03e8af901ab9b8ea54d26
lawsgb <glaws1009@yahoo.com>
2020-08-17 13:33:08 -0500

    second commit

--- JsonplaceholderAPI.test.js
+++ JsonplaceholderAPI.test.js
@@ -1,46 +1,56 @@
 const request = require('supertest')
 
 describe('Perform GET request to get all comments from a post', () => {
-    it('responds with a 200 status code', (done) => {
+    it('responds with json and 200 status code', (done) => {
         const endpoint = 'https://jsonplaceholder.typicode.com/posts/1'
         request(endpoint)
             .get('/comments')
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
             .expect(200, done)
     })
 })
 
 describe('Perform GET request to get all photos from an album', () => {
-    it('responds with a 200 status code', (done) => {
+    it('responds with json and 200 status code', (done) => {
         const endpoint = 'https://jsonplaceholder.typicode.com/albums/1'
         request(endpoint)
             .get('/photos')
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
             .expect(200, done)
     })
 })
 
 describe('Perform GET request to get all albums from a user', () => {
-    it('responds with a 200 status code', (done) => {
+    it('responds with json and 200 status code', (done) => {
         const endpoint = 'https://jsonplaceholder.typicode.com/users/1'
         request(endpoint)
             .get('/albums')
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
             .expect(200, done)
     })
 })
 
 describe('Perform GET request to get all todos from a user', () => {
-    it('responds with a 200 status code', (done) => {
+    it('responds with json and a 200 status code', (done) => {
         const endpoint = 'https://jsonplaceholder.typicode.com/users/1'
         request(endpoint)
             .get('/todos')
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
             .expect(200, done)
     })
 })
 
 describe('Perform GET request to get all posts from a user', () => {
-    it('responds with a 200 status code', (done) => {
+    it('responds with json and a 200 status code', (done) => {
         const endpoint = 'https://jsonplaceholder.typicode.com/users/1'
         request(endpoint)
             .get('/posts')
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
             .expect(200, done)
     })
 })
